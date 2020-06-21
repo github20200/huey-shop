014cc6b831947034d5a861bf6b13e77abc340bc3
phamgiahuyvtv <phamgiahuyvtv@gmail.com>
2020-06-22 00:05:31 +0700

    mes

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/oshop'));
+app.use(express.static(__dirname + '/dist/hueyshop'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/oshop/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/hueyshop/index.html'));
 });
 
 // default Heroku PORT
