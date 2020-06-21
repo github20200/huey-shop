b8abb817db41fb4cb8653018f8a8d431eebda3d1
phamgiahuyvtv <phamgiahuyvtv@gmail.com>
2020-06-22 01:32:04 +0700

    changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,13 @@
-import { fireBaseKey } from 'private/firebase-key';
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    ...fireBaseKey
+    apiKey: "AIzaSyAj9mWvr2W21vgoBTe0e7MCQBC1HlDLOzI",
+    authDomain: "huey-shop.firebaseapp.com",
+    databaseURL: "https://huey-shop.firebaseio.com",
+    projectId: "huey-shop",
+    storageBucket: "huey-shop.appspot.com",
+    messagingSenderId: "176227997336",
+    appId: "1:176227997336:web:7c7f5f1f83775bb37222ed",
+    measurementId: "G-HXR8FLLLKL"
   }
 };
